1e415e13ecf47bfe70887ed69f5830321801a590
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-28 20:11:36 +0000

    first save, not tested

--- models/base_model.py
+++ models/base_model.py
@@ -11,3 +11,16 @@ class BaseModel():
 
         self.id = str(uuid.uuid4())
 
+    def __str__(self):
+        print("[{}] ({}) {}".format(self.__class__, self.id, to_dict(self)))
+
+    @classmethod
+    def to_dict(cls):
+        tdn = self.__dict__
+        format_time = {
+            "updated_at": tdn['updated_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
+            "created_at": tdn['created_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
+        }
+        tdn.update("__class__": cls.__name__)
+        tdn.update(format_time)
+        return tdn

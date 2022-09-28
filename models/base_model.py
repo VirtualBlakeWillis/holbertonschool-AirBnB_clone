0383b71ea135d36306842c935ff44d111908c26f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-28 22:33:39 +0000

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,12 @@ class BaseModel():
         """ Initialize Base Model Object """
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
         print(self.created_at)
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
 
     def to_dict(self):
         tdn = self.__dict__

620057a86dc6044e056fa3cdc344da124bae97b4
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-29 19:54:32 +0000

    changed updated_at init

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
     def str_to_dt_obj(self, sr):
         print(" year = " + sr[0:4])
